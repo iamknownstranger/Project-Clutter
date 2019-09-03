12bf4050d08e0b8daa8967897977fc8e99f4d8b8
Known Stranger <iamtheknownstranger@gmail.com>
2019-09-03 12:17:11 +0530

    Update Clutter Manager.py
    Added Interface and other requirements for graphical selection of target folder

--- Clutter Manager.py
+++ Clutter Manager.py
@@ -1,7 +1,45 @@
 import os
 import shutil
 #The path of the directory which contains clutter to be organized
-dir_path = 'D:\Downloads'
+#modules required for base opertions
+import os
+import shutil
+#modules for interface creation
+from tkinter import filedialog
+from tkinter import *
+from colorama import Fore, Back, Style
+import time
+ #modules for command line interface
+from colorama import init 
+from termcolor import colored 
+
+init() 
+  
+print(Fore.GREEN + Style.BRIGHT + " #####                                                       #     #                                           \n"
+"#     #  #      #    # ##### ##### ###### #####              ##   ##   ##   #    #   ##    ####  ###### #####  \n"
+"#        #      #    #   #     #   #      #    #             # # # #  #  #  ##   #  #  #  #    # #      #    # \n"
+"#        #      #    #   #     #   #####  #    #    #####    #  #  # #    # # #  # #    # #      #####  #    # \n"
+"#        #      #    #   #     #   #      #####              #     # ###### #  # # ###### #  ### #      #####  \n"
+"#     #  #      #    #   #     #   #      #   #              #     # #    # #   ## #    # #    # #      #   #  \n"
+" #####   #####   ####    #     #   ###### #    #             #     # #    # #    # #    #  ####  ###### #    # \n")
+
+print(Fore.RED + 'Select the Folder you want to make clutter free!') 
+
+time.sleep(2)
+def get_folder():
+    # Allow user to select a directory and store it in global variable called folder_path
+    global folder_path
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+    return filename
+
+root = Tk()
+root.withdraw()
+folder_path = StringVar()
+#The path of the directory which contains clutter to be organized
+dir_path = get_folder()
+print(dir_path)
+
 #list_files contains individual files/directories in the source directory
 list_files = os.listdir(dir_path)
 for file in list_files:
